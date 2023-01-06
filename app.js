46826992304b1431768e8b42698a99895fb8692a
Eric <menaericdaniel@gmail.com>
2023-01-06 08:51:55 -0300

    add form register & login width styles mobile done

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = 3030;
 app.use(express.static(path.join(__dirname,  'public' )))
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
+app.get('/registrar',(req,res) => res.sendFile(path.join(__dirname, 'views', 'users','register.html')))
+app.get('/ingresar',(req,res) => res.sendFile(path.join(__dirname, 'views', 'users','login.html')))
 
 
 
